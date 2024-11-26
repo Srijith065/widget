af92fa2e8b84c6b395776770130889400ef24b91
SrijithV <srijith.v@quadrasystems.net>
2024-11-26 20:01:59 +0530

    modified

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Ensure static files are served from the correct directory
-app.use(express.static(path.join(__dirname)));
+// Serve static files from the root directory
+app.use(express.static(__dirname));
 
-// Serve index.html for the root URL and all routes
+// Always serve index.html for any route
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
