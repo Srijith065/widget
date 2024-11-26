a0885da016d8ea0308eb806b04cd48725d4ab05d
SrijithV <srijith.v@quadrasystems.net>
2024-11-26 19:08:23 +0530

    npm adds

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the "wwwroot" directory (Azure's default directory)
-app.use(express.static(path.join(__dirname, 'wwwroot')));
+// Serve static files from the root directory (if your files are not inside a folder like 'public')
+app.use(express.static(path.join(__dirname)));
 
 // Serve the index.html file for the root URL
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'wwwroot', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));  // Ensure it's pointing to the correct location of index.html
 });
 
 // Listen on the specified port
